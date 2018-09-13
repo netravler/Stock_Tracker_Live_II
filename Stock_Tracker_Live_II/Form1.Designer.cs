629156e0af3817980d523bb69a29c43df844a1a7
netravler <pheintz@hotmail.com>
2018-09-13 13:44:36 -0500

    Add files via upload

---
+++ Stock_Tracker_Live_II/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Stock_Tracker_Live_II
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(152, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(393, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Track these (seperate by ,)";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(15, 64);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(564, 89);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Gainsboro;
+            this.chart1.BorderlineColor = System.Drawing.Color.Maroon;
+            this.chart1.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(15, 176);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(852, 210);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            title1.Name = "Title1";
+            this.chart1.Titles.Add(title1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(578, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(254, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 536);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Stock_Tracker_Live_II";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Stock_Tracker_Live_II/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+//
+using System.Net;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Stock_Tracker_Live_II
+{
+    public partial class Form1 : Form
+    {
+        private Timer _Timer = new Timer();
+        public int chart1_advance = 1;
+        string[] holdticker;
+        string[] holdchart1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // chart stuff
+            ChartArea area = new ChartArea("First");
+            chart1.ChartAreas.Add(area);
+            chart1.Name="test";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _Timer.Interval = 6000;
+            _Timer.Tick += new EventHandler(_Timer_Tick);
+            _Timer.Start();
+
+            //name the chart once here
+
+            // parse for use in chart1
+            //string[] holdchart1 = holdstufffromcsv.Split('+');
+            
+            holdticker = textBox1.Text.Split('+');
+
+            foreach (string tickd in holdticker)
+            {
+                //if (chart1.Series.FindByName(holdchart1[4]) == null)
+                if (chart1.Series.FindByName(tickd) == null)
+                {
+                    //http://www.youtube.com/watch?v=82jnryBxsnI
+                    //http://www.codeproject.com/Articles/5431/A-flexible-charting-library-for-NET
+
+                    //Series series = this.chart1.Series.Add(holdchart1[4]);
+                    chart1.Series.Add(tickd).ChartType = SeriesChartType.Line;
+                }
+            }
+            // end of chart prep
+        }
+
+        void _Timer_Tick(object sender, EventArgs e)
+        {
+            // read into richtextbox
+            string holdchart1_old = ""; // prime for headings must not have duplicate...
+
+
+            foreach (string tickd in holdticker)
+            {
+                WebClient clientTXT = new WebClient();
+                var holdstufffromcsv = clientTXT.DownloadString("http://quote.yahoo.com/d/quotes.csv?s=" + tickd + "&f=snl1d1t1cv");
+                richTextBox1.Text += holdstufffromcsv;
+
+                // split for putting into a chart
+                //string[] holdchart1 = holdstufffromcsv.Split(',');
+                string[] holdchart1 = holdstufffromcsv.Split('"');
+
+                //holdticker = textBox1.Text.Split('\');
+
+                //Series series = this.chart1.Series.Add(holdchart1[4]);
+
+                holdchart1[4] = holdchart1[4].Replace(",","");
+
+                chart1.Series[tickd].Points.AddXY(holdchart1[7], Convert.ToDouble(holdchart1[4]));
+
+                clientTXT.Dispose();
+             }
+
+            
+        }
+
+    }
+}
